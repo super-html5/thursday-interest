ae8498ba6b9ca6ef1ff0d30d0caaca0f461f6397
nizhenkeai <1065800184@qq.com>
2018-05-22 15:17:03 +0800

    周四秒杀  修改样式 图片 线下调试地址

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,8 @@
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
+// const baseText = '/ccb-api';
+const baseText = '/cbc-api';
 
 export const environment = {
     production: false,
@@ -10,62 +12,62 @@ export const environment = {
     /**
      * 获取油券列表
      */
-    moneyUrl: '/ccb-api/api/v1/cbc/goods/query',
+    moneyUrl: `${baseText}/api/v1/cbc/goods/query`,
 
     /**
      * 添加订单
      */
-    addOilOrderUrl: '/ccb-api/api/v1/cbc/SecKillOrders',
+    addOilOrderUrl: `${baseText}/api/v1/cbc/SecKillOrders`,
 
     /**
      * 支付订单
      */
-    payOrderdUrl: '/ccb-api/api/v1/cashier/payOrder',
+    payOrderdUrl: `${baseText}/api/v1/cashier/payOrder`,
 
     /**
      * 发送认证信息卡验证码
      */
-    sendAuthMsg: `/ccb-api/api/v1/cbc/auth/sendAuthMsg`,
+    sendAuthMsg: `${baseText}/api/v1/cbc/auth/sendAuthMsg`,
 
     /**
      * 验证并保存信息卡
      */
-    authMsgUrl: `/ccb-api/api/v1/cbc/auth/authMsg`,
+    authMsgUrl: `${baseText}/api/v1/cbc/auth/authMsg`,
 
     /**
      * 查询是否绑定信息卡
      */
-    checkIsAuthUrl: `/ccb-api/api/v1/cbc/auth/checkIsAuth`,
+    checkIsAuthUrl: `${baseText}/api/v1/cbc/auth/checkIsAuth`,
     /**
      * 获取tokenId
      */
-    getTokenUrl: `/ccb-api/api/v1/cbc/account/getToken`,
+    getTokenUrl: `${baseText}/api/v1/cbc/account/getToken`,
 
     /**
      * 汽车养护券
      */
-    receiveVoucher: `/ccb-api/api/v1/cbc/SecKillOrders/giveCareCoupons`,
+    receiveVoucher: `${baseText}/api/v1/cbc/SecKillOrders/giveCareCoupons`,
     /**
      * 获取时间戳
      */
-    getTimeUrl: `/ccb-api/api/v1/cbc/time/nowTime`,
+    getTimeUrl: `${baseText}/api/v1/cbc/time/nowTime`,
 
     /**
      * 汽车养护 获取类别&类别下的商品
      */
-    classifyUrl: `/ccb-api/api/v1/cbc/carLife/category/list`,
+    classifyUrl: `${baseText}/api/v1/cbc/carLife/category/list`,
     /**
      * 汽车养护 下单
      */
-    addCarOrderUrl: `/ccb-api/api/v1/cbc/carLife/orders/add`,
+    addCarOrderUrl: `${baseText}/api/v1/cbc/carLife/orders/add`,
     /**
      * 汽车养护 判断订单是否能够支付
      */
-    judgeCarOrderdUrl: `/ccb-api/api/v1/cbc/carLife/orders/judge`,
+    judgeCarOrderdUrl: `${baseText}/api/v1/cbc/carLife/orders/judge`,
     /**
      * 汽车养护 确认收货
      */
-    carOrderConfirm: `/ccb-api/api/v1/cbc/carLife/orders/confirm`,
+    carOrderConfirm: `${baseText}/api/v1/cbc/carLife/orders/confirm`,
 
   }
 ;
